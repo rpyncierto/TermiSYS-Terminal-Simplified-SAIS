e10196aca0284febc627f03bcdcdc6f87eb35a0e
Reymond <ynciertorp@gmail.com>
2023-01-31 21:32:33 +0800

    feat: Added reset password

--- login.py
+++ login.py
@@ -1,4 +1,6 @@
 from loadData import users;
+import getpass;
+from display import clearScreen, welcomeMessage;
 
 def usernameExists(username):
     if username in users:
@@ -9,3 +11,23 @@ def passwordCheck(username, password):
     if (users[username] == password):
         return True;
     return False;
+    
+def resetPassword(database, username):
+    newPassword = getpass.getpass("Enter new password: ")
+    confirmPassword = getpass.getpass("Confirm new password: ")
+    print();
+    if len(newPassword) < 8:
+        clearScreen();
+        welcomeMessage();
+        print(">> Password must be at least 8 characters. Please try again!\n")
+        return resetPassword(database, username);
+    elif newPassword == confirmPassword:
+        users[username] = newPassword
+        database.write(username + "," + newPassword)
+        print(">> Password changed successfully!")
+    else:
+        clearScreen();
+        welcomeMessage();
+        print(">> Password did not match. Please try again!\n")
+        return resetPassword(database, username);
+    
